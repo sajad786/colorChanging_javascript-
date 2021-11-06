b06f11d6f3c3240c0004d3383606db1035045a9e
Sajad Hussain <sajad1820375@gmail.com>
2021-11-07 01:43:21 +0530

    color changing on hover with js

---
+++ js/color.js
@@ -0,0 +1,31 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+
+
+const center = document.querySelector(".center");
+
+// console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBgColor = (selectedElement) => {
+    return window.getComputedStyle(selectedElement).backgroundColor;
+}
+
+// var color = getBgColor(orange);
+
+// orange.addEventListener("mouseenter", () => {
+//     center.style.background = color;
+// });
+
+const magicColorChanger =( element, color) => {
+    element.addEventListener("mouseenter", () => {
+        center.style.background = color;
+    }); 
+}
+magicColorChanger(red, getBgColor(red));
+magicColorChanger(cyan, getBgColor(cyan));
+magicColorChanger(violet, getBgColor(violet));
+magicColorChanger(orange, getBgColor(orange));
+magicColorChanger(pink, getBgColor(pink));
